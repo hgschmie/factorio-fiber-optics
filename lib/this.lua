a4a13f4a77fad84aea0859b1784653bc18d0d191
Henning P. Schmiedehausen <henning@schmiedehausen.org>
2024-11-28 22:26:05 -0800

    remove space exploration support (not ported to 2.0)

--- lib/this.lua
+++ lib/this.lua
@@ -7,7 +7,6 @@ local This = {
     other_mods = {
         PickerDollies = 'PickerDollies',
         ['even-pickier-dollies'] = 'PickerDollies',
-        ['space-exploration'] = 'space-exploration'
     },
     debug_mode = 0, -- bit 0 (0/1): network debug, bit 1 (0/2): entity debug
 
