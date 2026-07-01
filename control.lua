528d6c097bc2df2b55aea339a6c97b768417ffa6
Henning P. Schmiedehausen <henning@schmiedehausen.org>
2026-06-30 21:54:18 -0700

    fix blueprint flipping bug

--- control.lua
+++ control.lua
@@ -38,21 +38,17 @@ local function on_entity_created(event)
     local tags = event.tags
     local player_index = event.player_index
 
-    local config, h_flipped, v_flipped = This.fo:deserialize(tags)
+    local config, h_flipped, v_flipped = nil, false, false
 
-    if not config then
-        -- normal build
+    -- see if a ghost (with tags) from a blueprint is replaced
+    local entity_ghost = Framework.Ghost:findGhostForEntity(entity)
+    if entity_ghost then
+        tags = tags or entity_ghost.tags
+        player_index = player_index or entity_ghost.player_index
+    end
 
-        -- see if a ghost (with tags) from a blueprint is replaced
-        local entity_ghost = Framework.Ghost:findGhostForEntity(entity)
-        if entity_ghost then
-            tags = tags or entity_ghost.tags
-            player_index = player_index or entity_ghost.player_index
-        end
-
-        if tags then
-            config, h_flipped, v_flipped = This.fo:deserialize(tags)
-        end
+    if tags then
+        config, h_flipped, v_flipped = This.fo:deserialize(tags)
     end
 
     -- fix up older blueprint configurations
